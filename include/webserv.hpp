d9168ec9209b1a586d717f718b361f5facfdeedc
jmazoyer <jmazoyer@student.42.fr>
2022-11-10 16:14:51 +0100

    RM useless "webserv.hpp"

--- include/webserv.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef WEBSERV_HPP
-#define WEBSERV_HPP
-
-#include "utils/Logger.hpp"
-
-#define	LOG(level, msg)	\
-webserv::Logger::getInstance().log(__FILE__, __LINE__, level, msg)
-
-#endif /* WEBSERV_HPP */
