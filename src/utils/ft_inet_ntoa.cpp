0f57cab6b1068c910de96128faa9dffe2b0c6b16
jmazoyer <jmazoyer@student.42.fr>
2022-11-10 16:50:17 +0100

    ADD "ft_inet_ntoa.cpp"
    - Reproduce the "inet_ntoa" function to record the clients' IP addresses

---
+++ src/utils/ft_inet_ntoa.cpp
@@ -0,0 +1,22 @@
+#include <sstream>
+#include <string>
+
+#include <netinet/in.h>
+
+namespace	webserv
+{
+
+	const char*	ft_inet_ntoa(struct in_addr in)
+	{
+		std::ostringstream		stream;
+		unsigned char*			bytes = reinterpret_cast<unsigned char*>(&in);
+		static std::string		str;
+
+		stream << static_cast<int>(bytes[0]) << "." << static_cast<int>(bytes[1])
+			<< "." << static_cast<int>(bytes[2])
+			<< "." << static_cast<int>(bytes[3]);
+		str = stream.str();
+		return (str.c_str());
+	}
+
+}	// namespace webserv
