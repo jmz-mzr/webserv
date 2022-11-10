a2263bcc2639859ebbe1e917f2b8b2ab4786b162
jmazoyer <jmazoyer@student.42.fr>
2022-11-10 15:35:42 +0100

    MOD memset
    - Rename the file with the actual function name

---
+++ src/utils/ft_memset.cpp
@@ -0,0 +1,16 @@
+#include <stddef.h>
+
+namespace	webserv
+{
+
+	void*	ft_memset(void* s, int c, size_t n)
+	{
+		char*	str;
+
+		str = (char*)s;
+		while (n--)
+			*str++ = c;
+		return (s);
+	}
+
+}	// namespace webserv
