6b31be2c6ebfc94b8101ad0d92ee9069d5e9cdfd
flohrel <flohrel@student.42.fr>
2022-11-29 01:19:47 +0100

    Update Parser

--- include/config/Parser.hpp
+++ include/config/Parser.hpp
@@ -1,18 +1,64 @@
 #ifndef PARSER_HPP
 #define PARSER_HPP
 
+#include <list>
+#include <stack>
+#include <string>
+#include <vector>
+
+#include "config/Lexer.hpp"
+#include "config/Location.hpp"
+#include "config/Config.hpp"
+
 namespace	webserv {
 
 namespace	config {
-
-	class	ConfigParser;
-	class	Lexer;
 
 	class Parser {
 		public:
 			Parser();
 			~Parser();
+
+			struct Directive {
+				enum Type {
+					kServerBlock,
+					kLocationBlock,
+					kServerName,
+					kListen,
+					kErrorPage,
+					kLimitExcept,
+					kRoot,
+					kIndex,
+					kBodyBufSize,
+					kReturn,
+					kAutoindex,
+					kFastcgi
+				}				type;
+				enum DupRule {
+					kIgnore,
+					kForbid,
+					kAccept
+				}				dupRule;
+				enum Context {
+					kServer,
+					kLocation,
+					kBoth
+				}				context;
+				size_t			argc;
+			};
+
+			void	operator()(token_queue& tokens);
+
+			void	createServer();
+			void	createLocation();
+
+
+
 		private:
+			Token*								_currToken;
+			Location*							_currLocation;
+			ServerConfig*						_currServer;
+			std::map<std::string, Directive>	_syntaxRules;
 	};
 
 }	// namespace config
