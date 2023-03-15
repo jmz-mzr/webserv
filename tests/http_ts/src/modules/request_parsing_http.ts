6ac3d2e5f9e981b53ddce0fce92c78e656921af3
Masylk <togbemaxime@gmail.com>
2023-03-15 11:34:07 +0100

    Viable version of tester with http modules

--- tests/http_ts/src/modules/request_parsing_http.ts
+++ tests/http_ts/src/modules/request_parsing_http.ts
@@ -4,6 +4,8 @@ import * as http from 'http'
 import { parseState, Request, Response } from '../http'
 import { TestData, ITestSuite } from '../types'
 
+
+
 export const TestSuite: ITestSuite = {
 	name: "request_parsing",
 	init() {
@@ -27,58 +29,53 @@ export const TestSuite: ITestSuite = {
 		for (let param in suiteFixture) {
 			currTest[param] = suiteFixture[param];
 		}
-		// for (let testCase of testCases) {
-		// 	for (let param in testCase) {
-		// 		currTest[param] = testCase[param];
-		// 	}
-		// 	let options = currTest;
-
-		// 	console.log(currTest);
-		// 	let req = http.request(options, (res)=>{
-		// 		console.log(`STATUS: ${res.statusCode}`);
-		// 		console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-		// 		res.setEncoding('utf8');
-		// 		res.on('data', (chunk) => {
-		// 		  console.log(`BODY: ${chunk}`);
-		// 		});
-		// 		res.on('end', () => {
-		// 		  console.log('No more data in response.');
-		// 		});
-		// 	});
-		//}
 		
-		const postData = JSON.stringify({
-			'msg': 'Hello World!',
-		});
-
-		const options = {
-			hostname: 'localhost',
-			port: 8080,
-			path: '/',
-			method: 'GET',
-			headers: {
-			  'Content-Type': 'application/json',
-			  'Content-Length': Buffer.byteLength(postData),
-			},
-		  };
-		  console.log("OUI");
-		const req = http.request(options, (res) => {
-			console.log(`STATUS: ${res.statusCode}`);
-			console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-			res.setEncoding('utf8');
-			res.on('data', (chunk) => {
-			  console.log(`BODY: ${chunk}`);
+		for (let testCase of testCases)
+		{
+			for (let param in testCase) {
+				currTest[param] = testCase[param];
+			}
+			const postData = JSON.stringify({
+				'msg': 'Hello World!',
 			});
-			res.on('end', () => {
-			  console.log('No more data in response.');
-			});
-		  });
-
-		  req.on('response', (e) => {
-			console.error(`problem with request: ${e.message}`);
-		  });
-		req.write(postData);
-		req.end();
-		console.log(req);
+			const options = {
+				hostname: 'localhost',
+				port: 8080,
+				path: '/',
+				method: 'GET',
+				headers: {
+				  'Content-Type': 'application/json',
+				  'Content-Length': Buffer.byteLength(postData),
+				  'Host~': 'oui',
+				},
+			  };
+			const req = http.request(options, (res) => {
+				/* Print Headers
+					console.log(`HEADERS: ${JSON.stringify(res.headers)}`);*/
+				let output: string = `${testCase.name}: `;
+				if (testCase.expectedCode === res.statusCode) {
+					output += '\x1b[32mSUCCESS\x1b[0m';
+				} else {
+					output += `\x1b[31mFAIL\x1b[0m Expected: ${testCase.expectedCode} | Actual: \x1b[31m${res.statusCode}\x1b[0m`;
+				}
+				res.setEncoding('utf8');
+				// Need to listen for the data event or else the callback will infinite loop
+				res.on('data', (chunk) => {
+				//	console.log(`BODY: ${chunk}`);
+				});
+				/*res.on('end', () => {
+				  console.log('No more data in response.');
+				});*/
+				console.log(output);
+			  });
+	
+			  req.on('error', (e) => {
+				console.error(`problem with request: ${e}`);
+			  });
+			req.write(postData);
+			req.end();
+		//	console.log(req);
+		}
+		
 	}
 }
