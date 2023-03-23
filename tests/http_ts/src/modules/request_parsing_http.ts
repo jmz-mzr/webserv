1937e09eef1b06869e34e3b9e3198417f5988295
Masylk <togbemaxime@gmail.com>
2023-03-23 21:45:52 +0100

    working on tester

--- tests/http_ts/src/modules/request_parsing_http.ts
+++ tests/http_ts/src/modules/request_parsing_http.ts
@@ -3,7 +3,7 @@ import * as net from 'net'
 import * as http from 'http'
 import { parseState, Request, Response } from '../http'
 import { TestData, ITestSuite } from '../types'
-
+import assert from 'assert'
 
 // Example of a request configuration
 // const options = {
@@ -23,59 +23,156 @@ export const TestSuite: ITestSuite = {
 		if (!workdir) {
 			throw new Error('Cannot find current working directory');
 		}
-		const filePath: string = path.join(workdir, 'data/request_parsing copy.json');
+		const filePath: string = path.join(workdir, 'data/request_parsing_debug.json');
 		if (!filePath) {
 			throw new Error('Cannot find test configuration file');
 		}
 		this.data = require(filePath);
 	},
 	execute() {
-		if (!this.data) {
-			throw new Error('Cannot parse configuration file');
-		}
-		let { suiteFixture, testCases } = this.data;
-		let currTest = new TestData();
-
-		for (let param in suiteFixture) {
-			currTest[param] = suiteFixture[param];
-		}
-		
-		for (let testCase of testCases)
+		const	compareStatusCode = (data1:any, data2: any, res1:any, res2: any, output: any) =>
 		{
-			for (let param in testCase) {
-				currTest[param] = testCase[param];
+			output += "Status code test : ";
+			if (res2.statusCode === res1.statusCode) {
+				output += '\x1b[32mSUCCESS\x1b[0m \n';
+			} else {
+				output += `\x1b[31mFAIL\n\x1b[0mExpected: ${res2.statusCode} | Actual: \x1b[31m${res1.statusCode}\x1b[0m \n`;
 			}
-			const postData = JSON.stringify({
-				'msg': 'Hello World!',
-			});
-		
-			const req = http.request(currTest, (res) => {
-				/* Print Headers
-					console.log(`HEADERS: ${JSON.stringify(res.headers)}`);*/
-				let output: string = `${testCase.name}: `;
-				if (testCase.expectedCode === res.statusCode) {
-					output += '\x1b[32mSUCCESS\x1b[0m';
-				} else {
-					output += `\x1b[31mFAIL\x1b[0m Expected: ${testCase.expectedCode} | Actual: \x1b[31m${res.statusCode}\x1b[0m`;
-				}
-				res.setEncoding('utf8');
-				// Need to listen for the data event or else the callback will infinite loop
-				res.on('data', (chunk) => {
-				//	console.log(`BODY: ${chunk}`);
-				});
-				/*res.on('end', () => {
-				  console.log('No more data in response.');
-				});*/
-				console.log(output);
-			  });
-	
-			  req.on('error', (e) => {
-				console.error(`problem with request: ${e}`);
-			  });
-			req.write(postData);
-			req.end();
-		//	console.log(req);
+			console.log(output);
 		}
+		const	compareHeaders = (data1:any, data2: any, res1:any, res2: any, output: any) =>
+		{
+			// Compare headers
+			output += "Headers test : ";
+			const headers1 = JSON.stringify(res1.headers, null, '\n');
+			const headers2 = JSON.stringify(res2.headers, null, '\n');
+			assert.deepEqual(res1.headers, res2.headers, `\x1b[31mFAIL\n\x1b[0mExpected: ${headers2}\nActual: \x1b[31m${headers1}\x1b[0m \n`)
+			//Determine headers to compare
+			output += '\x1b[32mSUCCESS\x1b[0m \n';
+			console.log(output);
+		}
+		const	compareBody = (data1:any, data2: any, res1:any, res2: any, output: any) =>
+		{
+			// Compare response body
+			output += "Compare body test : "
+			if (data1 === data2) {
+				output += '\x1b[32mSUCCESS\x1b[0m \n';
+			} else {
+					output += `\x1b[31mFAIL\n\x1b[0mExpected: ${data2} \nActual: \x1b[31m${data1}\x1b[0m`;
+			}
+			console.log(output);
+		}
+		const	compareCookies = (data1:any, data2: any, res1:any, res2: any, output: any) =>
+		{
 		
+			const cookie1 = res1.headers['set-cookie'];
+			const cookie2 = res2.headers['set-cookie'];
+			if (JSON.stringify(cookie1) === JSON.stringify(cookie2)) {
+			console.log('Cookies match');
+			} else {
+				console.log('Cookies do not match');
+			}
+			console.log(output);
+		}
+		const	compareTime = (data1:any, data2: any, res1:any, res2: any, output: any) =>
+		{
+			// const timeDiff = Math.abs(new Date(res1.headers.date) - new Date(res2.headers.date));
+			// if (timeDiff < 100) {
+			// 	console.log('Response times are similar');
+			// } else {
+			// 	console.log('Response times are different');
+			// }
+			console.log(output);
+		}
+		const	compareError = (data1:any, data2: any, res1:any, res2: any, output: any) =>
+		{
+			// Compare error messages
+			// if (res1.statusCode !== 200 && res2.statusCode !== 200) {
+			// 	output += "Error message test : \n";
+			// 	const error1 = JSON.parse(data1);
+			// 	const error2 = JSON.parse(data2);
+			// 	if (error1 === error2) {
+			// 	output += '\x1b[32mSUCCESS\x1b[0m';
+			// 	} else {
+			// 		output += `\x1b[31mFAIL\n\x1b[0mExpected: ${error2} \nActual:\x1b[31m${error1}\x1b[0m`;
+			// 	}
+			// }
+			console.log(output);
+		}
+
+		const 	compareServers = (testCase: any, currTest: any, server: any) =>
+		{
+			return new Promise<void>(resolve =>
+			{
+
+				const req = http.request(currTest, (res1) => {
+					let output: string = "";
+					let	data1 = ' ';
+					res1.on('data', (chunk) => {
+						data1 = chunk;
+					});
+					res1.on('end', () => {
+						currTest["host"] = "localhost";
+						currTest["port"] = 80;
+						currTest["headers"] = {Host: 'localhost:80'};
+						const req2 = http.request(currTest, (res2) => {
+							let	data2 = ' ';
+							res2.on('data', (chunk2) => {
+								data2 = chunk2;
+							});
+							res2.on('end', () => {
+								console.log(`${testCase.name}: \n`);
+								console.log(res2.rawHeaders);
+								console.log(res1.rawHeaders);
+								compareStatusCode(data1, data2, res1, res2, output);
+								compareHeaders(data1, data2, res1, res2, output);
+								compareBody(data1, data2, res1, res2, output);
+								// compareError(data1, data2, res1, res2, output);
+								// compareCookies(data1, data2, res1, res2, output);
+								resolve();
+							});
+						});
+						req2.on('error', (e) => {
+							console.error(`problem with request: ${e}`);
+						});
+						req2.end();
+					});
+					res1.setEncoding('utf8');
+				});
+				req.on('error', (e) => {
+					console.error(`problem with request: ${e}`);
+				});
+				req.end();
+			});
+		}
+			
+		const doTests = async () => {
+			
+			if (!this.data) {
+				throw new Error('Cannot parse configuration file');
+			}
+			
+			//const server = http.createServer((req, res) => {res.end();});
+			//server.listen(3000, 'localhost', () => {});
+			const server = "";
+			let { suiteFixture, testCases } = this.data;
+			for (let testCase of testCases)
+			{
+				let currTest = new TestData();
+			
+				for (let param in suiteFixture) {
+					currTest[param] = suiteFixture[param];
+				}	
+				for (let param in testCase) {
+					currTest[param] = testCase[param];
+				}
+				await compareServers(testCase, currTest, server);
+			}	
+			// server.close(()=>{
+			// 	console.log("Default Server closed");
+			// });
+		}
+
+		doTests();
 	}
 }
