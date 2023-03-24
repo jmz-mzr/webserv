846213c266742063da4c44441e5a633ccf24440b
Masylk <togbemaxime@gmail.com>
2023-03-24 21:44:38 +0100

    add some utils for python tester

--- tests/http_py/main.py
+++ tests/http_py/main.py
@@ -1,6 +1,7 @@
 import socket
 from http.client import HTTPResponse
 from enum import Enum
+import standard_tests as t_headers
 
 HOST = "localhost"
 PORT = 8080
@@ -9,22 +10,27 @@ class Status(Enum):
     FAIL = False
     SUCCESS = True
 
-def send_request(string: str) -> HTTPResponse :
+def send_request(string: str) -> tuple[HTTPResponse, socket.socket] :
+    print("Sending Request: \n" + string);
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
     s.send(string.encode())
     response = HTTPResponse(s)
-    return response
+    return (response, s)
 
 error_msg = ""
 
 def test_get() -> Status:
-    request = "GET / HTTP/1.1\r\nHost: localhost:8080\r\n\r\n"
-    response = send_request(request)
+    request = {"requestline": "GET / HTTP/1.1", "Host":  "localhost:8080"}
+    response, sock = send_request(t_headers.dict_to_request(request))
     if response.getcode() != 200:
         error_msg = "Bad status code {}, required {}".format(response.getcode(), 200)
         return Status.FAIL
+    t_headers.check_redirection(response, sock);
+    if (t_headers.test_headers(response) == False):
+        error_msg += "Headers do not match"
     return Status.SUCCESS
 
 if __name__ == "__main__":
     test_get()
+    print("Result: \n" + error_msg)
