5759d09caadafa18e618744ba911df314ddecde6
flohrel <flohrel@student.42.fr>
2023-04-02 15:00:59 +0200

    Update python tester

--- tests/http_py/modules/chunked.py
+++ tests/http_py/modules/chunked.py
@@ -13,8 +13,8 @@ class Chunked(TestCase):
             "0\r\n\r\n"
         ]
         response = send_request(*request)
-        if response.status != 201:
-            return f"Status code: {response.status}, expected: 201"
+        if response.status != 201 and response.status != 303:
+            return f"Status code: {response.status}, expected: 201 | 303"
         return ""
 
     @staticmethod
@@ -25,153 +25,89 @@ class Chunked(TestCase):
             "0\r\naccept-language: fr\r\ntest-header: blabla\r\n\r\n"
         ]
         response = send_request(*request)
-        if response.status != 201:
-            return f"Status code: {response.status}, expected: 201"
+        if response.status != 201 and response.status != 303:
+            return f"Status code: {response.status}, expected: 201 | 303"
         return ""
 
-    #
-    # @staticmethod
-    # def test_chunked_multiple_zeros() -> str:
-    #     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #     client.connect((SERVER_HOST, SERVER_PORT))
-    #     request_header = "POST /post/print.cgi HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n\r\n"
-    #     client.send(request_header.encode())
-    #     request_header = "5\r\ntest\n\r\n"
-    #     client.send(request_header.encode())
-    #     request_header = "00000\r\n\r\n"
-    #     client.send(request_header.encode())
-    #
-    #     # read and parse http response
-    #     http_response = HTTPResponse(client)
-    #     http_response.begin()
-    #     if http_response.status != 201 and http_response.status != 303:
-    #         return "Status code: {}, expected: {}".format(
-    #             str(http_response.status), "201"
-    #         )
-    #     # print(body)
-    #     return ""
-    #
-    # @staticmethod
-    # def test_chunk_extension_no_value() -> str:
-    #     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #     client.connect((SERVER_HOST, SERVER_PORT))
-    #     request_header = "POST /post/print.cgi HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n\r\n"
-    #     client.send(request_header.encode())
-    #     request_header = "5\r\ntest\n\r\n000;dude\r\n\r\n"
-    #     client.send(request_header.encode())
-    #
-    #     # read and parse http response
-    #     http_response = HTTPResponse(client)
-    #     http_response.begin()
-    #     if http_response.status != 201 and http_response.status != 303:
-    #         return "Status code: {}, expected: {}".format(
-    #             str(http_response.status), "201"
-    #         )
-    #     # print(body)
-    #     return ""
-    #
-    # @staticmethod
-    # def test_ChunkExtensionUnquotedValue() -> str:
-    #     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #     client.connect((SERVER_HOST, SERVER_PORT))
-    #     request_header = "POST /post/print.cgi HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n\r\n"
-    #     client.send(request_header.encode())
-    #     request_header = "5\r\ntest\n\r\n"
-    #     client.send(request_header.encode())
-    #     request_header = "000;dude=best\r\n\r\n"
-    #     client.send(request_header.encode())
-    #
-    #     # read and parse http response
-    #     http_response = HTTPResponse(client)
-    #     http_response.begin()
-    #     if http_response.status != 201 and http_response.status != 303:
-    #         return "Status code: {}, expected: {}".format(
-    #             str(http_response.status), "201"
-    #         )
-    #     # print(body)
-    #     return ""
-    #
-    # @staticmethod
-    # def test_multiple_chunk_extensions() -> str:
-    #     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #     client.connect((SERVER_HOST, SERVER_PORT))
-    #     request_header = "POST /post/print.cgi HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n\r\n"
-    #     client.send(request_header.encode())
-    #     request_header = '000;Foo=Bar;dude="The dude is the best";Spam=12345!\r\n\r\n'
-    #     client.send(request_header.encode())
-    #
-    #     # read and parse http response
-    #     http_response = HTTPResponse(client)
-    #     http_response.begin()
-    #     if http_response.status != 201 and http_response.status != 303:
-    #         return "Status code: {}, expected: {}".format(
-    #             str(http_response.status), "201"
-    #         )
-    #     # print(body)
-    #     return ""
-    #
-    # @staticmethod
-    # def test_decode_empty_body_in_pieces() -> str:
-    #     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #     client.connect((SERVER_HOST, SERVER_PORT))
-    #     request_header = "POST /post/print.cgi HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n\r\n"
-    #     client.send(request_header.encode())
-    #     request_header = "0"
-    #     client.send(request_header.encode())
-    #     request_header = "\r"
-    #     client.send(request_header.encode())
-    #     request_header = "\n"
-    #     client.send(request_header.encode())
-    #     request_header = "\r"
-    #     client.send(request_header.encode())
-    #     request_header = "\n"
-    #     client.send(request_header.encode())
-    #
-    #     # read and parse http response
-    #     http_response = HTTPResponse(client)
-    #     http_response.begin()
-    #     if http_response.status != 201 and http_response.status != 303:
-    #         return "Status code: {}, expected: {}".format(
-    #             str(http_response.status), "201"
-    #         )
-    #     # print(body)
-    #     return ""
-    #
-    # @staticmethod
-    # def test__decode_three_chunks_one_piece() -> str:
-    #     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #     client.connect((SERVER_HOST, SERVER_PORT))
-    #     request_header = "POST /post/print.cgi HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n\r\n"
-    #     client.send(request_header.encode())
-    #     request_header = "F\r\nHello, World!!!\r\n7\r\nIt's me\r\n0\r\n\r\n"
-    #     client.send(request_header.encode())
-    #
-    #     # read and parse http response
-    #     http_response = HTTPResponse(client)
-    #     http_response.begin()
-    #     if http_response.status != 201 and http_response.status != 303:
-    #         return "Status code: {}, expected: {}".format(
-    #             str(http_response.status), "201"
-    #         )
-    #     body = http_response.read().decode("UTF-8")
-    #     if body.find("Hello, World!!!It's me") == -1:
-    #         return "Missing content 'Hello, World!!!It's me' "
-    #     return ""
-    #
-    # @staticmethod
-    # def test_decode_bad_trailer() -> str:
-    #     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    #     client.connect((SERVER_HOST, SERVER_PORT))
-    #     request_header = "POST /post/print.cgi HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n\r\n"
-    #     client.send(request_header.encode())
-    #     request_header = "0\r\nX-Foo Bar\r\n\r\n"
-    #     client.send(request_header.encode())
-    #
-    #     # read and parse http response
-    #     http_response = HTTPResponse(client)
-    #     http_response.begin()
-    #     if http_response.status != 400:
-    #         return "Status code: {}, expected: {}".format(
-    #             str(http_response.status), "400"
-    #         )
-    #     return ""
+    @staticmethod
+    def test_multiple_zeros() -> str:
+        request = [
+            "POST /post/multzero HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n",
+            "5\r\ntest\n\r\n",
+            "00000\r\n\r\n"
+        ]
+        response = send_request(*request)
+        if response.status != 201 and response.status != 303:
+            return f"Status code: {response.status}, expected: 201 | 303"
+        return ""
+
+    @staticmethod
+    def test_extension_no_value() -> str:
+        request = [
+            "POST /post/extnoval HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n",
+            "5\r\ntest\n\r\n000;dude\r\n\r\n"
+        ]
+        response = send_request(*request)
+        if response.status != 201 and response.status != 303:
+            return f"Status code: {response.status}, expected: 201 | 303"
+        return ""
+
+    @staticmethod
+    def test_extension_unquoted_value() -> str:
+        request = [
+            "POST /post/extunquotval HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n",
+            "5\r\ntest\n\r\n",
+            "000;dude=best\r\n\r\n"
+        ]
+        response = send_request(*request)
+        if response.status != 201 and response.status != 303:
+            return f"Status code: {response.status}, expected: 201 | 303"
+        return ""
+
+    @staticmethod
+    def test_multiple_extensions() -> str:
+        request = [
+            "POST /post/multext HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n",
+            '000;Foo=Bar;dude="The dude is the best";Spam=12345!\r\n\r\n'
+        ]
+        response = send_request(*request)
+        if response.status != 201 and response.status != 303:
+            return f"Status code: {response.status}, expected: 201 | 303"
+        return ""
+
+    @staticmethod
+    def test_empty_body_multiple_send() -> str:
+        request = [
+            "POST /post/emptymultsend HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n",
+            "0",
+            "\r",
+            "\n",
+            "\r",
+            "\n"
+        ]
+        response = send_request(*request)
+        if response.status != 201 and response.status != 303:
+            return f"Status code: {response.status}, expected: 201 | 303"
+        return ""
+
+    @staticmethod
+    def test_one_send() -> str:
+        request = [
+            "POST /post/basic2 HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n",
+            "F\r\nHello, World!!!\r\n7\r\nIt's me\r\n0\r\n\r\n"
+        ]
+        response = send_request(*request)
+        if response.status != 201 and response.status != 303:
+            return f"Status code: {response.status}, expected: 201 | 303"
+        return ""
+
+    @staticmethod
+    def test_bad_trailer() -> str:
+        request = [
+            "POST /post/ HTTP/1.1\r\nHost: localhost\r\nTransfer-encoding: chunked\r\n",
+            "0\r\nX-Foo Bar\r\n\r\n"
+        ]
+        response = send_request(*request)
+        if response.status != 400:
+            return f"Status code: {response.status}, expected: 400"
+        return ""
