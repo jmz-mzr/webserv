cb9c346abc1d6f92a341c1a3b45654b6fe075b46
jmazoyer <jmazoyer@student.42.fr>
2023-04-16 13:01:14 +0200

    ADD googletest libraries

---
+++ tests/googletest/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/julienmazoyer/42/projects/webserv/code/tests/googletest
+# Build directory: /Users/julienmazoyer/42/projects/webserv/code/tests/googletest/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
